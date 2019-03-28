cb366f9cadc955f45eb61dda735df01003296078
rafeDraper <rafedraper@gmail.com>
2019-03-28 13:22:24 +0100

    update eval crap

--- java.js
+++ java.js
@@ -38,19 +38,23 @@ function factorial(input) {
 }
 ////// NOTE EQUAL FUNCTION
 
-let equal = (input) => {
+let equal = (a, op, b) => {
 	switch (op) {
 		case `+`:
 			return add(a, b);
+			break;
 		case `-`:
 			return subtract(a, b);
+			break;
 		case `*`:
 			return multiply(a, b);
+			break;
 		case `/`:
 			return divide(a, b);
+			break;
 
 	}
-};
+}
 
 
 // SECTION Manipulate the screen
@@ -58,7 +62,6 @@ let equal = (input) => {
 let getHistory = () => {
 	return document.getElementById(`anotation`).innerText;
 }
-
 
 let printHistory = (input) => {
 	document.getElementById(`anotation`).innerText = input;
@@ -86,29 +89,29 @@ let reverseNumberFormat = (input) => {
 	return Number(input.replace(/[.,]/g,''));
 }
 
-var operator = document.getElementsByClassName("operator");
-for(var i =0;i<operator.length;i++){
+let operator = document.getElementsByClassName("operator");
+for(let i =0;i<operator.length;i++){
 	operator[i].addEventListener('click',function(){
 		if(this.id=="clear"){
 			printHistory("");
 			printOutput("");
 		}
-		else if(this.id=="delete"){
-			var output=reverseNumberFormat(getOutput()).toString();
+		else if(this.id =="delete"){
+			let output=reverseNumberFormat(getOutput()).toString();
 			if(output){//if output has a value
 				output= output.substr(0,output.length-1);
 				printOutput(output);
 			}
 		}
 		else{
-			var output=getOutput();
-			var history=getHistory();
-			if(output==""&&history!=""){
+			let output = getOutput();
+			let history = getHistory();
+			if(output == "" && history != ""){
 				if(isNaN(history[history.length-1])){
 					history= history.substr(0,history.length-1);
 				}
 			}
-			if(output!="" || history!=""){
+			if(output != "" || history != ""){
 				output= output==""?output:reverseNumberFormat(output);
 				history=history+output;
 				console.log(history)
